e2219a47a28370c7a9b0e6b8cce9317525e55e36
Kristine Pham <kristineph101@gmail.com>
2025-04-20 15:26:33 -0400

    render learn page

--- server.py
+++ server.py
@@ -3,6 +3,56 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
+drinks = [
+    { "id": 0,
+    "name": "Espresso",
+    "img": "../static/images/espresso.png",
+    "ingredients": ["1 part espresso"],
+    "taste_profile": "Bitter",
+    "similar_drinks": ["Macchiato", "Americano"]
+    },
+    
+    { "id": 1,
+    "name": "Latte",
+    "img": "../static/images/latte.png",
+    # TODO: should it be 2 parts of 1 part, 1 part?
+    "ingredients": ["1 part espresso", "2 parts steamed milk", "A thin layer of foamed milk"],
+    "taste_profile": "Milky",
+    "similar_drinks": ["Flat White", "Cappuccino"]
+    },
+    
+    { "id": 2,
+    "name": "Cappuccino",
+    "img": "../static/images/cappuccino.png",
+    "ingredients": ["1 part espresso", "1 part steamed milk", "1 part foamed milk"],
+    "taste_profile": "Milky",
+    "similar_drinks": ["Flat White", "Latte"]
+    },
+
+    { "id": 3,
+    "name": "Americano",
+    "img": "../static/images/americano.png",
+    "ingredients": ["1 part espresso", "2 parts water"],
+    "taste_profile": "Bitter",
+    "similar_drinks": ["Espresso", "Macchiato"]
+    },
+
+    { "id": 4,
+    "name": "Flat White",
+    "img": "../static/images/flat_white.png",
+    "ingredients": ["1 part espresso", "2 parts steamed milk"],
+    "taste_profile": "Milky",
+    "similar_drinks": ["Latte", "Cappuccino"]
+    },
+
+    { "id": 5,
+    "name": "Macchiato",
+    "img": "../static/images/macchiato.png",
+    "ingredients": ["2 parts espresso", "A splash of steamed milk"],
+    "taste_profile": "Bitter",
+    "similar_drinks": ["Espresso", "Americano"]
+    }
+]
 
 @app.route('/')
 def home():
@@ -10,7 +60,11 @@ def home():
 
 @app.route('/learn/<id>')
 def learn(id):
-    return render_template('learn_item.html')
+    return render_template('learn_item.html', item = drinks[int(id)-1])
+
+@app.route('/make/<id>')
+def make(id):
+    return render_template('make_item.html', item = drinks[int(id)-1])
 
 if __name__ == '__main__':
     app.run(debug = True, port=5001)
