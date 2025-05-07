ac6bbb602d13a3e602a7853911eb5e89941398a2
Kristine Pham <kristineph101@gmail.com>
2025-05-07 11:02:56 -0400

    output mc feedback

---
+++ static/multiple_choice.js
@@ -0,0 +1,27 @@
+mc_answers = [0, 1, 2, 3, 0, 1]
+
+$(document).ready(function(){
+
+    console.log(mc_answers[3])
+    $('.mc-button').click(function() {
+        $("#feedback").empty()
+        $("#next-button-col").empty()
+
+        // get drink number we are on
+        drink_num = parseInt($(location).attr('href').slice(-1))
+
+        // get the id of mc
+        mc_id = parseInt((this.id))
+        console.log(mc_id)
+
+        // compare with correct answer
+        if (mc_id == mc_answers[drink_num] ) {
+            $('#feedback').html("Correct!")
+            $('#next-button-col').append('<a href="/make/'+ drink_num + '"><button id="next-button">Next</button></a>')
+        } else {
+            $('#feedback').html("Incorrect! Try Again.")
+        }
+
+    });
+
+})
